09c40243019d5340508739353368dc1ccb24e908
jimzhu <jmzhu.sz@gmail.com>
2015-08-27 14:53:42 +0100

    revise setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='comonitor',
+    name='CoMonitor',
     description='An python package for compressive monitoring',
     long_description=open('README.md').read(),
     version='0.1dev',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     install_requires=['numpy'],
-    url='https://github.com/cuhk-cse/comonitor',
-    author='CUHK-CSE',
-    author_email='CUHK-CSE@Github'
+    url='https://github.com/cuhk-cse/CoMonitor',
+    author='CUHK-CSE'
 )
